7554b2db5ed3c6bf731ec9c1d909dbc1cf624836
Sergio Cárdenas <s.cardenas@raboit.com>
2015-07-08 01:25:11 -0300

    variables and templates

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,10 @@
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
+    mynames: [
+      { propername: "This is name #1" },
+      { propername: "This is name #2" },
+      { propername: "This is name #3" }
     ]
   });
 }
